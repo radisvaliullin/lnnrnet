646c5d6275f30104b277d04933f646a23803ac54
Radis Valiullin <radisrower@gmail.com>
2024-03-11 02:42:04 -0700

    bk0: ch3: tools, add quadratic and cross entropy cost functions

--- bk0/src/tools.py
+++ bk0/src/tools.py
@@ -9,3 +9,39 @@ def sigmoid(z):
 def sigmoid_prime(z):
     """Derivative of the sigmoid function"""
     return sigmoid(z)*(1-sigmoid(z))
+
+# Quadratic Cost Function
+class QuadraticCost:
+
+    @staticmethod
+    def fn(a, y):
+        """
+        Return the cost associated with an output "a" and desired output
+        "y".
+        """
+        return 0.5*np.linalg.norm(a-y)**2
+
+    @staticmethod
+    def delta(z, a, y):
+        """
+        Return the error delta from the output layer.
+        """
+        return (a-y) * sigmoid_prime(z)
+
+# Cross-Entropy Cost Function
+class CrossEntropyCost:
+
+    @staticmethod
+    def fn(a, y):
+        """
+        Return the cost associated with an output "a" and desired output
+        "y".
+        """
+        return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
+
+    @staticmethod
+    def delta(z, a, y):
+        """
+        Return the error delta from the output layer.
+        """
+        return (a-y)
