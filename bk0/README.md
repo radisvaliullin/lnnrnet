bd658c8ba47e477a7cf30f1040aaece5034b6e24
Radis Valiullin <radisrower@gmail.com>
2024-03-11 02:42:04 -0700

    bk0: readme: add info about download mnist

--- bk0/README.md
+++ bk0/README.md
@@ -1,5 +1,18 @@
 # book 0
-Nielsen Neural Networks Book
+Nielsen Neural Networks
 
 ## Python Version
 3.12.1
+
+## mnist
+download mnist:
+```
+wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+```
+put downloaded files to:
+```
+./bk0/data
+```
--- bk0/helper/mnist_decode_example.py
+++ bk0/helper/mnist_decode_example.py
@@ -1,4 +1,10 @@
 # simpel code showing how to decode mnist raw format
+# download mnist:
+# wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+# wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+# wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+# wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+# put downloaded files to ./bk0/data folder
 import gzip
 import numpy as np
 import matplotlib.pyplot as plt
